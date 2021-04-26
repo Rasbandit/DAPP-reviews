54b9b248ee3377c382c40b6add53eb2da0a1d2df
Todd Rasband <todd.rasband@podium.com>
2021-04-26 16:05:47 -0600

    Meets erc20 spec

--- src/contracts/SchloadCoin.sol
+++ src/contracts/SchloadCoin.sol
@@ -3,29 +3,67 @@ pragma solidity ^0.6.0;
 contract SchloadCoin {
     string public constant name = "Schload Coin";
     string public constant symbol = "SCHD";
-    uint256 public totalSupply = 100000; // 1 million tokens
-    uint8 public constant decimals = 2;
+    uint256 public totalSupply_ = 1000000000000000; // 1 million tokens
+    uint8 public constant decimals = 9;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
 
-    mapping(address => uint256) public balanceOf;
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) allowed;
 
     constructor() public {
-        balanceOf[msg.sender] = totalSupply;
+        balances[msg.sender] = totalSupply_;
     }
 
     function totalSupply() public view returns (uint256) {
-      return totalSupply;
+        return totalSupply_;
     }
 
-    function transfer(address _to, uint256 _value)
+    function balanceOf(address tokenOwner) public view returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address owner, address spender)
         public
-        returns (bool success)
+        view
+        returns (uint256)
     {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+        return allowed[owner][spender];
+    }
+
+    function approve(address authorizedAddress, uint256 numTokens)
+        public
+        returns (bool)
+    {
+        allowed[msg.sender][authorizedAddress] = numTokens;
+        emit Approval(msg.sender, authorizedAddress, numTokens);
+        return true;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool success) {
+        require(balances[msg.sender] >= value);
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(
+        address owner,
+        address buyer,
+        uint256 numTokens
+    ) public returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+        balances[owner] -= numTokens;
+        allowed[owner][msg.sender] = allowed[owner][msg.sender] - numTokens;
+        balances[buyer] = balances[buyer] + numTokens;
+        emit Transfer(owner, buyer, numTokens);
         return true;
     }
 }
